4bda2db599378c3b410c6045d88c79bb90a1c14d
김희망 <gmlakd990514@naver.com>
2024-10-21 09:46:50 +0900

    init: init

---
+++ src/main/java/com/nhnacademy/ssacthree_gateway/SsacthreeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ssacthree_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacthreeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsacthreeGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/ssacthree_gateway/SsacthreeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ssacthree_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsacthreeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
