069daa2f101a48bbd9aee93eef0facf5cc6cf457
김희망 <gmlakd990514@naver.com>
2024-11-01 00:11:21 +0900

    fix: JWT 관련된 것 제거

--- src/main/java/com/nhnacademy/ssacthree_gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/ssacthree_gateway/config/RouteLocatorConfig.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.ssacthree_gateway.config;
 
-import com.nhnacademy.ssacthree_gateway.filter.JwtAuthorizationFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +9,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouteLocatorConfig {
     @Bean
-    public RouteLocator myRoute(RouteLocatorBuilder builder, JwtAuthorizationFilter jwtAuthorizationFilter) {
+    public RouteLocator myRoute(RouteLocatorBuilder builder) {
         return builder.routes()
                 //해당 경로로 요청이 오면, 각 서비스로 로드밸런싱(lb)
                 .route("shop-service",
                         p->p.path("/api/shop/**")
-//                                .filters(f -> f.filter(jwtAuthorizationFilter.apply(new JwtAuthorizationFilter.Config()))) //jwt 토큰 검증
                                 .uri("lb://SHOP-SERVICE")
                 )
                 .route("auth-service",
